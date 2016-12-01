47ea872fcd135030ff9322dc4abb173cc498c862
rudvy9 <rudvy9@hanmail.net>
2016-12-01 10:18:26 +0900

    STL practice ok

--- [3611][practice06]_201311273_박경표/main.cpp
+++ [3611][practice06]_201311273_박경표/main.cpp
@@ -1,11 +1,14 @@
 #include <process.h>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
 #include "Game.h"
 #include "KeyEvent.h"
-#include "Frame.h"
-#include "ExArray.h"
-/*
+
 using namespace std;
+/*
 unsigned int __stdcall mythread(void*)
 {
 	int i = 0;
@@ -60,20 +63,14 @@ unsigned int __stdcall keyEvent(void*)
 	return 0;
 }
 
-*/
-template<typename T>
-void mySwap(T& num1, T& num2)
-{
-	T temp = num1;
-	num1 = num2;
-	num2 = temp;
-
-};
 
 int main()
 {
-/*	
-HANDLE handleA, handleB;
+	
+
+	
+
+	HANDLE handleA, handleB;
 
 	handleA = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
 	handleB = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
@@ -86,49 +83,119 @@ HANDLE handleA, handleB;
 
 	return 0;
 
-	try 
+}
+*/
+
+void dump(list<string> &l)
+{
+	list<string>::iterator iter = l.begin();
+
+	while (iter != l.end())
 	{
-		Frame *f1 = new Frame;
-		Frame * f2 = new Frame[10];
-
-		delete f1;
-		delete[] f2;
+		cout << *iter << endl;
+		iter++;
 	}
-	catch (string msg)
+}
+
+class Word
+{
+private:
+	string word;
+public:
+	Word(string word)
 	{
-		cout << "Error msg :" << msg << endl;
-
+		this->word = word;
 	}
-	*/
+	string getWord()
+	{
+		return this->word;
+	}
 
-	ExArray<int> arr1(3);
-	ExArray<char> arr2(4);
-	ExArray<double> arr3(5);
+};
 
-	arr1.addData(2);
-	arr2.addData('A');
-	arr3.addData(32.12);
+void main()
+{
+	/*
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.insert(v.begin(), 40);
+	v.insert(v.end(), 50);
 
-	arr1.printData();
-	arr2.printData();
-	arr3.printData();
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+	cout << "access index at 1 : " << v.at(1) << endl;
+	v.erase(v.begin());
+	v.erase(v.end() - 1);
 
-	int num1 = 10, num2 = 40;
-	cout << "before : " << num1 << ", " <<num2<< endl;
-	mySwap<int>(num1, num2);
-	cout << "after : " << num1 << ", " << num2<<endl;
+	vector<int>::iterator iter = v.begin();
 
-	double num1d = 121.11, num2d = 10.5;
-	cout << "before : " << num1d << ", " <<num2d<< endl;
-	mySwap<double>(num1d, num2d);
-	cout << "after : " << num1d << ", " <<num2d<<endl;
+	cout << "access index at 1 with iterator : " << iter[1] << endl;
 
-	ExArray<int> arr4(5);
-	arr4.addData(10);
-
-	mySwap<ExArray<int>>(arr1, arr4);
-	arr1.printData();
-	arr4.printData();
+	while (iter != v.end())
+	{
+		cout << *iter << endl;
+		iter++;
+	}
 	
+
+	list<string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.end(), "University");
+	names.insert(names.end(), "SCLAB");
+	names.insert(names.end(), "CSY");
+	dump(names);
+	cout << "===" << endl;
+	names.reverse();
+	dump(names);
+
+	map<string, int> m;
+
+	m["seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul" << m["seoul"] << "$" << endl;
+	cout << "Train to Daegu" << m["daegu"] << "$" << endl;
+	cout << "Train to Busan" << m["busan"] << "$" << endl;
+	*/
+	map<char, list<Word>> m;
+	
+	list<Word> aList;
+	list<Word> bList;
+
+	aList.push_back(Word("apple"));
+	aList.push_back(Word("avoid"));
+	aList.push_back(Word("appear"));
+
+	bList.push_back(Word("bread"));
+	bList.push_back(Word("bring"));
+	bList.push_back(Word("bow"));
+
+	m['a'] = aList;
+	m['b'] = bList;
+
+	map<char, list<Word>>::const_iterator iter = m.begin();
+
+	while (iter != m.end())
+	{
+		cout << iter->first << " : " << endl;
+
+		list<Word> temp = iter->second;
+		list<Word>::const_iterator iter_list = temp.begin();
+
+		while (iter_list != temp.end())
+		{
+			Word word = *iter_list;
+			cout << word.getWord() << " , ";
+			iter_list++;
+		}
+		cout << "\n===" << endl;
+		iter++;
+
+	}
 
 }
